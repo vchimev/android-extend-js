4507fc9b2d8be189a357ff87dc6fbee6439c7fc7
Vasil Chimev <Vasil.Chimev@gmail.com>
2018-06-14 19:42:52 +0300

    Add application to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = env => {
     const appComponents = [
         "tns-core-modules/ui/frame",
         "tns-core-modules/ui/frame/activity",
+        "./application.android",
     ];
 
     const platform = env && (env.android && "android" || env.ios && "ios");
